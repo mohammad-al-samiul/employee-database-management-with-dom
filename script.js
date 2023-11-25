0e73b0ec2fbcedf12d21888c6f8cf0ae4d1690b5
Noob-H4cker <alsamiul123@gmail.com>
2023-11-25 22:30:03 +0600

    fetch data employee

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hi");
+async function EmployeeFetchDate() {
+    const res = await fetch('./data.json');
+    const data = await res.json();
+    console.log(data);
+}
+EmployeeFetchDate();

549d85cf123f76b49a2e3ffba9e958fc5fd795a1
Noob-H4cker <alsamiul123@gmail.com>
2023-11-29 22:14:03 +0600

    delete employee and create employee

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// (async function () {
-//   const res = await fetch("./data.json");
-//   const data = await res.json();
-
-//   const employees = data;
-//   let selectedEmployeeId = employees[0].id;
-//   let selectedEmployee = employees[0];
-
-//   const employeeList = document.querySelector(".employee_name_list");
-
-//   const renderEmployees = () => {
-//     employeeList.innerHTML = "";
-//     employees.forEach((emp) => {
-//         const employee = document.createElement("span");
-//         employee.classList.add("employee-list-item");
-
-//         if(parseInt(selectedEmployeeId) === emp.id){
-//             employee.classList.add("selected");
-//             selectedEmployee = emp;
-//         }
-//         employee.setAttribute("id", emp.id);
-//         employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">❌</i> <br/>`;
-//         employeeList.append(employee);
-//     });
-//   };
-
-//   renderEmployees();
-// })();
-
 (async function () {
   const res = await fetch("./data.json");
   const data = await res.json();
@@ -41,6 +12,46 @@
   const employeeList = document.querySelector(".employee-name-list");
   const employeeInfo = document.querySelector(".employee-single-info");
 
+  const createEmployee = document.querySelector(".createEmployee");
+  const addEmployeeModal = document.querySelector(".addEmployee");
+  const addEmployeeForm = document.querySelector(".addEmployee_create");
+
+  createEmployee.addEventListener("click", () => {
+    addEmployeeModal.style.display = "flex";
+  });
+
+  addEmployeeModal.addEventListener("click", (e) => {
+    if (e.target.className === "addEmployee") {
+      addEmployeeModal.style.display = "none";
+    }
+  });
+
+  // Set Employee age to be entered minimum 18 years
+  const dobInput = document.querySelector(".addEmployee_create--dob");
+  dobInput.max = `${new Date().getFullYear() - 18}-${new Date()
+    .toISOString()
+    .slice(5, 10)}`;
+
+  addEmployeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(addEmployeeForm);
+    const values = [...formData.entries()];
+    // console.log(values);
+    let empData = {};
+    values.forEach((val) => {
+      empData[val[0]] = val[1];
+    });
+    //console.log(empData);
+    empData.id = employees[employees.length - 1].id + 1;
+    empData.age = new Date().getFullYear() - parseInt(empData.dob.slice(0, 4), 10);
+    empData.imageUrl = empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+    employees.push(empData);
+    renderEmployees();
+    addEmployeeForm.reset();
+    addEmployeeModal.style.display = "none";
+  });
+
   employeeList.addEventListener("click", (e) => {
     if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
       selectedEmployeeId = e.target.id;
@@ -48,6 +59,18 @@
       renderEmployees();
       renderSingleEmployee();
     }
+
+    // Employee Delete Logic - START
+    if (e.target.tagName === "I") {
+      employees = employees.filter(
+        (emp) => String(emp.id) !== e.target.parentNode.id
+      );
+      if (String(selectedEmployeeId) === e.target.parentNode.id) {
+        selectedEmployeeId = employees[0]?.id || -1;
+        selectedEmployee = employees[0] || {};
+        renderSingleEmployee();
+      }
+      renderEmployees();
   });
 
   const renderEmployees = () => {
@@ -70,6 +93,12 @@
   };
 
   const renderSingleEmployee = () => {
+    // Employee Delete Logic - START
+    if (selectedEmployeeId === -1) {
+      employeeInfo.innerHTML = "";
+      return;
+    }
+     // Employee Delete Logic - END
     employeeInfo.innerHTML = `
     <div class="flex flex-col items-center jusify-center">
     <img class="w-56" src="${selectedEmployee.imageUrl}" alt="">

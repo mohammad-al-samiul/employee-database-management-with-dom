8b604c39a1e84fcf9d93837028ae55c8ed284d88
Noob-H4cker <alsamiul123@gmail.com>
2023-11-29 12:07:17 +0600

    employee single info completed

--- script.js
+++ script.js
@@ -36,9 +36,19 @@
   let selectedEmployee = employees[0];
   //console.log(selectedEmployee);
   let selectedEmployeeId = employees[0].id;
-  //   console.log(selectedEmployeeId);
+  console.log("employee id : ", selectedEmployeeId);
 
   const employeeList = document.querySelector(".employee-name-list");
+  const employeeInfo = document.querySelector(".employee-single-info");
+
+  employeeList.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
+      selectedEmployeeId = e.target.id;
+      console.log("selected employee id : ", selectedEmployeeId);
+      renderEmployees();
+      renderSingleEmployee();
+    }
+  });
 
   const renderEmployees = () => {
     employeeList.innerHTML = "";
@@ -49,14 +59,34 @@
       if (parseInt(selectedEmployeeId) === emp.id) {
         employee.classList.add("selected");
         selectedEmployee = emp;
+        console.log("selected employee : ", selectedEmployee);
       }
+
+      employee.setAttribute("id", emp.id);
 
       employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">❌</i> <br/>`;
       employeeList.append(employee);
-
-
     });
   };
+
+  const renderSingleEmployee = () => {
+    employeeInfo.innerHTML = `
+    <div class="flex flex-col items-center jusify-center">
+    <img class="w-56" src="${selectedEmployee.imageUrl}" alt="">
+    <div class="flex flex-col ">
+    <span>Name : ${selectedEmployee.firstName} ${selectedEmployee.lastName} </span>
+    <span>age : ${selectedEmployee.age}</span>
+    <span>contact : ${selectedEmployee.contactNumber}</span>
+    <span>email : ${selectedEmployee.email}</span>
+    <span>address : ${selectedEmployee.address}</span>
+    </div>
+    </div>
+    `;
+  };
+
+  if (selectedEmployee) {
+    renderSingleEmployee();
+  }
 
   renderEmployees();
 })();
